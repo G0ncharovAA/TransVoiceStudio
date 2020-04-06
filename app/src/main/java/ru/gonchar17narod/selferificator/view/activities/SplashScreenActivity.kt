193f2016e66bbceb9164a347d4a0d0965eab17fd
abmin <AlxAlxGoncharov@gmail.com>
2020-04-06 16:38:38 +0300

    permission check added to SplashActivity

--- app/src/main/java/ru/gonchar17narod/selferificator/view/activities/SplashScreenActivity.kt
+++ app/src/main/java/ru/gonchar17narod/selferificator/view/activities/SplashScreenActivity.kt
@@ -1,11 +1,16 @@
 package ru.gonchar17narod.selferificator.view.activities
 
 import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import ru.gonchar17narod.selferificator.R
 
 class SplashScreenActivity : AppCompatActivity() {
+
+    private val PERMISSION_MICRO_CODE = 119
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,7 +19,43 @@ class SplashScreenActivity : AppCompatActivity() {
 
     override fun onResume() {
         super.onResume()
+        checkPermissions()
+    }
 
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            PERMISSION_MICRO_CODE -> {
+                if (grantResults.firstOrNull() == PackageManager.PERMISSION_GRANTED) {
+                    proceedToNextDestination()
+                } else {
+                    finish()
+                }
+            }
+        }
+    }
+
+    private fun checkPermissions() {
+        if (ContextCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.RECORD_AUDIO
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            proceedToNextDestination()
+        } else {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(android.Manifest.permission.RECORD_AUDIO),
+                PERMISSION_MICRO_CODE
+            )
+        }
+    }
+
+    private fun proceedToNextDestination() {
         startActivity(
             Intent(
                 this,
