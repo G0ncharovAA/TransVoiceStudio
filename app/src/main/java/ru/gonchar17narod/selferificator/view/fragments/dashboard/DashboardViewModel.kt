bf1ce0e91f9c934c5a496b71fb12d0b66ad20cdf
abmin <AlxAlxGoncharov@gmail.com>
2020-04-06 15:46:08 +0300

    partially moved from view to viewmodel

--- app/src/main/java/ru/gonchar17narod/selferificator/view/fragments/dashboard/DashboardViewModel.kt
+++ app/src/main/java/ru/gonchar17narod/selferificator/view/fragments/dashboard/DashboardViewModel.kt
@@ -3,11 +3,72 @@ package ru.gonchar17narod.selferificator.view.fragments.dashboard
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import net.galmiza.android.engine.sound.SoundEngine
+import ru.gonchar17narod.selferificator.data.SpectreRepository
 
 class DashboardViewModel : ViewModel() {
+
+    val samplingRate = 44100
+    val fftResolution =
+    //1024
+        //2048
+        4096
+    //8192
+    private val continuousRecord =
+        SpectreRepository(samplingRate)
+    val soundEngine = SoundEngine().apply {
+        initFSin()
+    }
+
+    // Buffers
+    var bufferStack = mutableListOf<ShortArray>() // Store trunks of buffers
+    var fftBuffer = ShortArray(fftResolution) // buffer supporting the fft process
+    var re = FloatArray(fftResolution) // buffer holding real part during fft process
+    var im = FloatArray(fftResolution) // buffer holding imaginary part during fft process
+
+    val liveTrunks = MutableLiveData<ShortArray>()
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is dashboard Fragment"
     }
     val text: LiveData<String> = _text
+
+    init {
+        loadEngine()
+    }
+
+    override fun onCleared() {
+        continuousRecord.stop()
+        continuousRecord.release()
+        super.onCleared()
+    }
+
+    fun startRecording() =
+        continuousRecord.start(
+            { buffer ->
+                liveTrunks.postValue(buffer)
+            }
+        )
+
+    fun stopRecording() =
+        continuousRecord.stop()
+
+    private fun loadEngine() =
+        with(continuousRecord) {
+
+            // Stop and release recorder if running
+            stop()
+            release()
+
+            // Prepare recorder
+            prepare(fftResolution) // Record buffer size if forced to be a multiple of the fft resolution
+
+            // Build buffers for runtime
+            val n = fftResolution
+
+            val l: Int = bufferLength / (n / 2)
+            for (i in 0 until l + 1) {  //+1 because the last one has to be used again and sent to first position
+                bufferStack.add(ShortArray(n / 2)) // preallocate to avoid new within processing loop
+            }
+        }
 }
