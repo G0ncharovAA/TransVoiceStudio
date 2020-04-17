18c66a369ca296aa140301c29403c7eda7c874ed
abmin <AlxAlxGoncharov@gmail.com>
2020-04-17 20:38:38 +0300

    alpha

--- app/src/androidTest/java/com/gmail/sasha/inverse/transvoicestudio/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/gmail/sasha/inverse/transvoicestudio/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("ru.gonchar17narod.selferificator", appContext.packageName)
+        assertEquals("com.gmail.sasha.inverse.transvoicestudio", appContext.packageName)
     }
 }
